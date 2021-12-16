65a2a6b18aa3a013055ce7cfc21a3c664b1220f4
wonseokb <70983771+wonseokb@users.noreply.github.com>
2021-12-15 23:11:27 -0600

    Add files via upload
    4. CMakeCache.txt, cmake_install.cmake, Makefile, and CMakeFiles directory are made when I tried "cmake .." on the build directory

---
+++ build/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wonseok/Project/Project.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/Project.cc.o"
+  "/home/wonseok/Project/src/Project1DetectorConstruction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project1DetectorConstruction.cc.o"
+  "/home/wonseok/Project/src/Project1Material1Sd.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project1Material1Sd.cc.o"
+  "/home/wonseok/Project/src/Project1MaterialHit1.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project1MaterialHit1.cc.o"
+  "/home/wonseok/Project/src/Project1PmtHit.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project1PmtHit.cc.o"
+  "/home/wonseok/Project/src/Project1PmtSd.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project1PmtSd.cc.o"
+  "/home/wonseok/Project/src/Project2EventAction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2EventAction.cc.o"
+  "/home/wonseok/Project/src/Project2PrimaryGeneratorAction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2PrimaryGeneratorAction.cc.o"
+  "/home/wonseok/Project/src/Project2PrimaryGeneratorMessenger.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2PrimaryGeneratorMessenger.cc.o"
+  "/home/wonseok/Project/src/Project2RunAction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2RunAction.cc.o"
+  "/home/wonseok/Project/src/Project2StackingAction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2StackingAction.cc.o"
+  "/home/wonseok/Project/src/Project2SteppingAction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2SteppingAction.cc.o"
+  "/home/wonseok/Project/src/Project2TrackingAction.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2TrackingAction.cc.o"
+  "/home/wonseok/Project/src/Project2Trajectory.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2Trajectory.cc.o"
+  "/home/wonseok/Project/src/Project2TrajectoryPoint.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2TrajectoryPoint.cc.o"
+  "/home/wonseok/Project/src/Project2UserEventInformation.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2UserEventInformation.cc.o"
+  "/home/wonseok/Project/src/Project2UserTrackInformation.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project2UserTrackInformation.cc.o"
+  "/home/wonseok/Project/src/Project3EmPhysics.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project3EmPhysics.cc.o"
+  "/home/wonseok/Project/src/Project3GeneralPhysics.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project3GeneralPhysics.cc.o"
+  "/home/wonseok/Project/src/Project3MuonPhysics.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project3MuonPhysics.cc.o"
+  "/home/wonseok/Project/src/Project3PhysicsList.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/Project3PhysicsList.cc.o"
+  "/home/wonseok/Project/src/RNGWrapper.cc" "/home/wonseok/Project/build/CMakeFiles/Project.dir/src/RNGWrapper.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/Geant4"
+  "/usr/local/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/Project.cc.o"
+  "CMakeFiles/Project.dir/src/Project1DetectorConstruction.cc.o"
+  "CMakeFiles/Project.dir/src/Project1Material1Sd.cc.o"
+  "CMakeFiles/Project.dir/src/Project1MaterialHit1.cc.o"
+  "CMakeFiles/Project.dir/src/Project1PmtHit.cc.o"
+  "CMakeFiles/Project.dir/src/Project1PmtSd.cc.o"
+  "CMakeFiles/Project.dir/src/Project2EventAction.cc.o"
+  "CMakeFiles/Project.dir/src/Project2PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Project.dir/src/Project2PrimaryGeneratorMessenger.cc.o"
+  "CMakeFiles/Project.dir/src/Project2RunAction.cc.o"
+  "CMakeFiles/Project.dir/src/Project2StackingAction.cc.o"
+  "CMakeFiles/Project.dir/src/Project2SteppingAction.cc.o"
+  "CMakeFiles/Project.dir/src/Project2TrackingAction.cc.o"
+  "CMakeFiles/Project.dir/src/Project2Trajectory.cc.o"
+  "CMakeFiles/Project.dir/src/Project2TrajectoryPoint.cc.o"
+  "CMakeFiles/Project.dir/src/Project2UserEventInformation.cc.o"
+  "CMakeFiles/Project.dir/src/Project2UserTrackInformation.cc.o"
+  "CMakeFiles/Project.dir/src/Project3EmPhysics.cc.o"
+  "CMakeFiles/Project.dir/src/Project3GeneralPhysics.cc.o"
+  "CMakeFiles/Project.dir/src/Project3MuonPhysics.cc.o"
+  "CMakeFiles/Project.dir/src/Project3PhysicsList.cc.o"
+  "CMakeFiles/Project.dir/src/RNGWrapper.cc.o"
+  "Project.pdb"
+  "Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
